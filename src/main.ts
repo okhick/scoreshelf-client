3688d8796d83f311a33a6ac37994acba99fba7a9
Oliver Hickman <oliver.hickman@hey.com>
2020-12-19 23:03:49 -0600

    tweaks to allow typescript builds

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,10 @@ import Vue from 'vue';
 import VueCompositionAPI from '@vue/composition-api';
 import App from './App.vue';
 import router from './router';
-import store from './store';
+
+// this store will complain until we get full TS support
+// @ts-ignore
+import store from './store/index.js';
 import VueCookies from 'vue-cookies';
 
 Vue.use(VueCompositionAPI);
