3a35d7ff3526445715200609474252c2f270e32a
Oliver Hickman <oliver.hickman@hey.com>
2021-01-16 16:14:05 -0500

    Edit and view modes of edit profile

--- src/components/dashboard/editProfile/EditProfile.vue
+++ src/components/dashboard/editProfile/EditProfile.vue
@@ -1,12 +1,17 @@
 <template>
   <div>
-    <div v-show="messages.success.profile" class="notification is-success is-light">
-      <button class="delete" @click="closeMessage('success', 'profile')"></button>
-      Your profile has been updated!
-    </div>
-
     <div class="columns">
-      <div class="column is-half">
+      <!-- View Mode -->
+      <div v-show="!editMode" class="column is-two-thirds namecard">
+        <p>
+          <span class="name"> {{ firstName.value }} {{ lastName.value }} </span>
+          <span class="display-name">({{ displayName.value }})</span>
+        </p>
+        <p class="bio" v-html="bio.value"></p>
+      </div>
+
+      <!-- Edit mode -->
+      <div class="column is-two-thirds" v-show="editMode">
         <div class="columns">
           <div class="column is-half">
             <label class="label">First Name</label>
@@ -44,10 +49,15 @@
       </div>
     </div>
 
+    <!-- Action buttons -->
+    <button class="button is-dark" @click="toggleEditMode" v-show="!editMode">
+      <strong>Edit</strong>
+    </button>
+
     <button
       class="button is-dark"
       :class="{ 'is-loading': isLoading }"
-      type="button"
+      v-show="editMode"
       @click="updateProfile"
     >
       <strong>Update</strong>
@@ -55,16 +65,15 @@
 
     <hr class="hr" />
 
+    <!-- TODO: Email change is a different process -->
     <label class="label">Email</label>
     <div class="field has-addons">
       <div class="control">
-        <input :disabled="email.disabled" class="input" type="text" v-model="email.value" />
+        <input disabled class="input" type="text" v-model="email.value" />
       </div>
       <div class="control">
         <a class="button is-primary">
           <font-awesome-icon icon="lock" v-show="email.disabled" />
-          <!-- TODO: Email change is a different process -->
-          <font-awesome-icon icon="unlock" v-show="!email.disabled" />
         </a>
       </div>
     </div>
@@ -82,18 +91,16 @@ import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import { onMounted, reactive, ref, toRefs, watchEffect } from '@vue/composition-api';
 library.add(faUnlock, faLock);
 
-interface IMessages {
-  [key: string]: {
-    [key: string]: boolean;
-  };
-}
-
 export default {
   components: {
     FontAwesomeIcon,
     TrixEditorComponent,
   },
   setup() {
+    const { useRefreshLogin, useSharetribeState, useUpdateCurrentUser } = useSharetribe();
+    const { SHARETRIBE, currentUser } = useSharetribeState;
+
+    // ========== Load and init data ==========
     const formData = reactive({
       firstName: {
         disabled: true,
@@ -116,13 +123,7 @@ export default {
         value: '',
       },
     });
-    const isLoading = ref(false);
     const dataHasLoaded = ref(false);
-    const messages: IMessages = reactive({
-      success: {
-        profile: false,
-      },
-    });
 
     onMounted(async () => {
       await useRefreshLogin();
@@ -134,21 +135,17 @@ export default {
       formData.bio.value = currentUser.value?.attributes.profile.bio || '';
       formData.email.value = currentUser.value?.attributes.email || '';
 
+      // Trix has issues when renedred before there's data
       dataHasLoaded.value = true;
     });
 
-    // methods
-    const { useRefreshLogin, useSharetribeState, useUpdateCurrentUser } = useSharetribe();
-    const { SHARETRIBE, currentUser } = useSharetribeState;
-
+    // ========== handle Trix ==========
     function handleNewContent(event: string) {
       formData.bio.value = event;
     }
 
-    function closeMessage(passFail: string, message: string) {
-      messages[passFail][message] = !messages[passFail][message];
-    }
-
+    // ========== update data ==========
+    const isLoading = ref(false);
     async function updateProfile() {
       isLoading.value = true;
       const res = await SHARETRIBE.value.currentUser.updateProfile({
@@ -158,20 +155,44 @@ export default {
         bio: formData.bio.value,
       });
       await useRefreshLogin();
-      messages.success.profile = true;
+      await useUpdateCurrentUser();
+      toggleEditMode();
       isLoading.value = false;
     }
 
+    // ========== toggle between edit and view modes ==========
+    const editMode = ref(false);
+    function toggleEditMode() {
+      editMode.value = !editMode.value;
+    }
+
+    // ==========
+
     return {
       ...toRefs(formData),
-      messages,
+      editMode,
       isLoading,
       handleNewContent,
-      closeMessage,
       currentUser,
       updateProfile,
       dataHasLoaded,
+      toggleEditMode,
     };
   },
 };
 </script>
+
+<style lang="scss" scoped>
+.namecard {
+  span.name {
+    font-size: 20px;
+    font-weight: 600;
+  }
+  span.display-name {
+    font-size: 16px;
+  }
+  p.bio {
+    padding: 12px;
+  }
+}
+</style>
